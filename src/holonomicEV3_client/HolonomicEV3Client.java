b5cd886b11fa09556d70fbd90ca6e5b5db7f4c13
viv-dev <viv.dev@protonmail.com>
2017-10-15 15:10:08 +1100

    Initial Commit

---
+++ src/holonomicEV3_client/HolonomicEV3Client.java
@@ -0,0 +1,17 @@
+package holonomicEV3_client;
+
+import net.java.games.input.Component;
+import net.java.games.input.Controller;
+import net.java.games.input.ControllerEnvironment;
+import net.java.games.input.Version;
+import net.java.games.input.EventQueue;
+import net.java.games.input.Event;
+
+public class HolonomicEV3Client
+{
+	public static void main(String[] args)
+	{
+		
+	}
+
+}
---
+++ src/holonomicEV3_server/HolonomicEV3Server.java
@@ -0,0 +1,12 @@
+package holonomicEV3_server;
+
+public class HolonomicEV3Server
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}

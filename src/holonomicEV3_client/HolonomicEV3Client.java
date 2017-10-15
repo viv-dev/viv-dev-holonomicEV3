cf7ca7e34e38c8da230996830c6996eda221dbe7
viv-dev <viv.dev@protonmail.com>
2017-10-15 23:33:38 +1100

    Initial functionality working with keyboard and playstation controller. Needs some tuning.

--- src/holonomicEV3_client/HolonomicEV3Client.java
+++ src/holonomicEV3_client/HolonomicEV3Client.java
@@ -1,101 +1,325 @@
 package holonomicEV3_client;
 
-
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+
+import net.java.games.input.Component;
+import net.java.games.input.Controller;
+import net.java.games.input.ControllerEnvironment;
 
 import java.awt.*;
 import java.awt.event.*;
 
+import javax.swing.*;
 
-public class HolonomicEV3Client extends Frame
+public class HolonomicEV3Client extends JFrame
 {
-	public static final int KEYBOARD = 2;
-	public static final int JOYSTICK = 3;
-	
-	public static final int FORWARD = 1;
-	public static final int LEFT = 1;
-	public static final int BACK = 1;
-	public static final int RIGHT = 1;
-	public static final int DOWN = 1;
-	public static final int ROT_LEFT = 1;
-	public static final int ROT_RIGHT = 1;
-		
 	public static final int CLOSE = 0;
-	
+	public static final int JOYSTICK = 1;
+	public static final int KEYBOARD = 2;
+
+	public static final int FORWARD = 10;
+	public static final int LEFT = 11;
+	public static final int RIGHT = 12;
+	public static final int BACKWARD = 13;
+	public static final int ROT_LEFT = 14;
+	public static final int ROT_RIGHT = 15;
+
 	public static final int PORT = 7360;
-	
+
+	private final double MAX_VALUE = 0.6;
+	private final int MAX_SPEED = 32;
+	private final int SAMPLE_RATE = 200;
+
 	public static void main(String[] args)
 	{
 		HolonomicEV3Client client = new HolonomicEV3Client();
 	}
-	
+
 	// UI Elements
-	private Button connectButton;
-	private TextField txtIPAddress;
-	private TextArea statusMessage;
-	
+	private JButton connectButton;
+	private JTextField txtIPAddress;
+	private JTextArea statusMessage;
+	private JComboBox inputChoice;
+
 	// Message sending
 	private Socket socket;
 	private DataOutputStream outStream;
-	
+	private boolean connected;
+
+	private Controller currentController;
+	private int currentControllerIndex;
+	private int prevControllerIndex;
+
+	// Input handling
+	private ArrayList<Controller> foundControllers;
+
 	public HolonomicEV3Client()
 	{
 		super("HolonomicEV3Client");
-		this.setSize(400, 300);
-		
+
+		buildGUI("10.0.1.1");
+
+		currentController = null;
+		currentControllerIndex = 0;
+		prevControllerIndex = 0;
+
+		foundControllers = new ArrayList<>();
+		searchForControllers();
+
+		processInputs();
+
+	}
+
+	public void buildGUI(String ip)
+	{
+		this.setSize(475, 225);
+
 		this.addWindowListener(new WindowAdapter()
 		{
 			public void windowClosing(WindowEvent e)
 			{
-				System.out.println("Ending Robot Client");
+				System.out.println("Ending EV3 Client");
 				disconnect();
 				System.exit(0);
 			}
 		});
-		
-		buildGui("127.0.0.1");
-		
-		this.setVisible(true);
-		this.setResizable(false);
-		this.setLocationRelativeTo(null);
-		
-		this.addKeyListener(new KeyboardListener());
-	}
-	
-	public void buildGui(String ip)
-	{
-		Panel mainPanel = new Panel(new BorderLayout());
+
+		JPanel mainPanel = new JPanel(new BorderLayout());
 		ControlListener controlListener = new ControlListener();
 
-		connectButton = new Button("Connect");
+		connectButton = new JButton("Connect");
 		connectButton.addActionListener(controlListener);
+		connectButton.addKeyListener(new KeyboardListener());
 
-		txtIPAddress = new TextField(ip, 16);
+		txtIPAddress = new JTextField(ip, 20);
 
-		statusMessage = new TextArea("STATUS: DISCONNECTED");
+		statusMessage = new JTextArea("STATUS: DISCONNECTED");
+		statusMessage.setPreferredSize(new Dimension(450, 100));
 		statusMessage.setEditable(false);
 
-		Panel north = new Panel(new FlowLayout(FlowLayout.LEFT));
+		inputChoice = new JComboBox();
+		inputChoice.addItem("Keyboard");
+
+		JPanel north = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		north.add(connectButton);
 		north.add(txtIPAddress);
 
-		Panel center = new Panel(new GridLayout(5, 1));
-		center.add(new Label("A-S-D to steer, W-X to move"));
+		JPanel center = new JPanel(new BorderLayout(2, 1));
 
-		Panel center4 = new Panel(new FlowLayout(FlowLayout.LEFT));
-		center4.add(statusMessage);
-		
-		center.add(center4);
+		JPanel choiceList = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		choiceList.add(new Label("Pick input source:"));
+		choiceList.add(inputChoice);
+
+		JPanel message = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		message.add(statusMessage);
+
+		center.add(choiceList, "North");
+		center.add(message, "Center");
 
 		mainPanel.add(north, "North");
 		mainPanel.add(center, "Center");
+
 		this.add(mainPanel);
+
+		this.setVisible(true);
+		this.setResizable(true);
+		this.setLocationRelativeTo(null);
+
 	}
-	
-	
-	
-	
+
+	private void processInputs()
+	{
+		while (true)
+		{
+			updateCurrentController();
+
+			while (connected)
+			{
+				updateCurrentController();
+				if (currentController != null)
+				{
+					updateFromController();
+				}
+
+				try
+				{
+					Thread.sleep(SAMPLE_RATE);
+				}
+				catch (InterruptedException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	private void updateFromController()
+	{
+		if (!currentController.poll())
+		{
+			currentController = null;
+			displayDisconnected();
+			return;
+		}
+
+		double[] sn = { 0, 0, 0, 0, 0, 0, 0 };
+
+		Component[] components = currentController.getComponents();
+
+		for (Component component : components)
+		{
+			double temp = (double)component.getPollData();
+			
+			if(temp > 0.0 && temp < 0.3)
+				temp = 0.0;
+				
+			if(temp < 0.0 && temp > - 0.3)
+				temp = 0.0;
+						
+			switch (component.getName())
+			{
+				case "X Axis":
+					sn[0] = temp;
+					break;
+				case "Y Axis":
+					sn[1] = temp;
+					break;
+				case "Z Axis":
+					sn[2] = temp;
+					break;
+				case "X Rotation":
+					sn[3] = temp;
+					break;
+				case "Y Rotation":
+					sn[4] = temp;
+					break;
+				case "Z Rotation":
+					sn[5] = temp;
+					break;
+				default:
+					break;
+			}
+		}
+
+		double x = sn[0] * MAX_VALUE;
+		double y = sn[1] * MAX_VALUE;
+		double spinning = sn[2] * MAX_VALUE * MAX_SPEED;
+		double speed = Math.sqrt(x * x + y * y) * MAX_SPEED;
+		double theta = Math.atan2(y, x); // direction
+		double angle = Math.toDegrees(theta) + 90;
+
+		if (angle < 0.0d)
+		{
+			angle += 360.0;
+		}
+		if (speed > MAX_SPEED)
+		{
+			speed = MAX_SPEED;
+		}
+		;
+		if (speed == 0 && spinning == 0)
+		{
+			angle = 0;
+		}
+		if (spinning > MAX_SPEED)
+		{
+			spinning = MAX_SPEED;
+		}
+		;
+		if (spinning < -MAX_SPEED)
+		{
+			spinning = -MAX_SPEED;
+		}
+
+		//System.out.println(Double.toString(sn[0]) + " " + Double.toString(sn[1]) + " " + Double.toString(sn[2]));
+		sendCommand(JOYSTICK, (int) speed, (int) angle, (int) spinning);
+		System.out.println(Double.toString(spinning));
+
+	}
+
+	private void sendCommand(int source, int angle, int r, int rotate)
+	{
+		if(connected) 
+		{
+			// Send coordinates to Server:
+			statusMessage.setText("status: SENDING command.");
+			String message = source + "@@" + angle + "@@" + r + "@@" + rotate;
+			try
+			{
+				outStream.writeUTF(message);
+			}
+			catch (IOException e)
+			{
+				statusMessage.setText("status: ERROR Problems occurred sending data.");
+			}
+			statusMessage.setText("status: Command SENT.");
+		}
+	}
+
+	private void searchForControllers()
+	{
+		Controller[] controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
+
+		for (int i = 0; i < controllers.length; i++)
+		{
+			Controller controller = controllers[i];
+
+			if (controller.getType() == Controller.Type.STICK || controller.getType() == Controller.Type.GAMEPAD
+					|| controller.getType() == Controller.Type.WHEEL
+					|| controller.getType() == Controller.Type.FINGERSTICK)
+			{
+				// Add new controller to the list of all controllers.
+				foundControllers.add(controller);
+
+				// Add new controller to the list on the window.
+				addControllerName(controller.getName() + " - " + controller.getType().toString() + " type");
+			}
+		}
+
+	}
+
+	private void addControllerName(String controllerName)
+	{
+		inputChoice.addItem(controllerName);
+	}
+
+	private void updateCurrentController()
+	{
+		prevControllerIndex = currentControllerIndex;
+		currentControllerIndex = getSelectedController();
+
+		if (currentControllerIndex != prevControllerIndex)
+		{
+			if (currentControllerIndex == 0)
+			{
+				currentController = null;
+			}
+			else if (currentControllerIndex > 0 && ((currentControllerIndex-1) < foundControllers.size()))
+			{
+				currentController = foundControllers.get(currentControllerIndex-1);
+				System.out.println("Reading input from: " + currentController.getName());
+			}
+			else
+			{
+				System.out.println("Invalid controller index!");
+			}
+		}
+
+	}
+
+	private int getSelectedController()
+	{
+		return inputChoice.getSelectedIndex();
+	}
+
+	public void displayDisconnected()
+	{
+		inputChoice.removeAllItems();
+		inputChoice.addItem("Controller disconnected!");
+	}
+
 	private void connect()
 	{
 		try
@@ -105,62 +329,89 @@ public class HolonomicEV3Client extends Frame
 			outStream = new DataOutputStream(socket.getOutputStream());
 			statusMessage.setText("STATUS: CONNECTED");
 			connectButton.setLabel("Disconnect");
-			//looping = true;
+			connected = true;
 		}
 		catch (Exception exc)
 		{
 			String statusMsg = "STATUS: FAILURE - Error establishing connection with server.\n";
 			statusMessage.setText(statusMsg);
 			System.out.println("Error: " + exc);
-		}	
+		}
 	}
-	
+
 	private void disconnect()
 	{
 		try
 		{
-			//sendCommand(CLOSE, 0, 0, 0);
+			sendCommand(CLOSE, 0, 0, 0);
 			socket.close();
 			connectButton.setLabel("Connect");
 			statusMessage.setText("STATUS: DISCONNECTED");
+			connected = false;
 		}
 		catch (Exception exc)
 		{
 			statusMessage.setText("STATUS: FAILURE Error closing connection with server.");
 			System.out.println("Error: " + exc);
-		}	
+		}
 	}
-	
+
 	private class KeyboardListener implements KeyListener
 	{
 		@Override
 		public void keyPressed(KeyEvent e)
 		{
-			switch(e.getKeyCode())
+			if (currentControllerIndex == 0)
 			{
-				default:
-					System.out.println(e.getKeyChar());
+				switch (e.getKeyCode())
+				{
+					case 87: // W
+						sendCommand(KEYBOARD, FORWARD, 0, 0);
+						System.out.println("Pressed " + e.getKeyChar());
+						break;
+					case 65: // A
+						sendCommand(KEYBOARD, LEFT, 0, 0);
+						System.out.println("Pressed " + e.getKeyChar());
+						break;
+					case 83: // S
+						sendCommand(KEYBOARD, BACKWARD, 0, 0);
+						System.out.println("Pressed " + e.getKeyChar());
+						break;
+					case 68: // D
+						sendCommand(KEYBOARD, RIGHT, 0, 0);
+						System.out.println("Pressed " + e.getKeyChar());
+						break;
+					case 81: // Q
+						sendCommand(KEYBOARD, ROT_LEFT, 0, 0);
+						System.out.println("Pressed " + e.getKeyChar());
+						break;
+					case 69: // E
+						sendCommand(KEYBOARD, ROT_RIGHT, 0, 0);
+						System.out.println("Pressed " + e.getKeyChar());
+						break;
+					default:
+						System.out.println(e.getKeyChar());
+				}
 			}
-			
+
 		}
 
 		@Override
 		public void keyReleased(KeyEvent e)
 		{
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
 		public void keyTyped(KeyEvent e)
 		{
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 	}
-	
-	/** Listens for click events on the connect/disconnect button */
+
 	private class ControlListener implements ActionListener
 	{
 		public void actionPerformed(ActionEvent e)
@@ -176,6 +427,5 @@ public class HolonomicEV3Client extends Frame
 			}
 		}
 	}
-
 
 }
--- src/holonomicEV3_server/HolonomicEV3Server.java
+++ src/holonomicEV3_server/HolonomicEV3Server.java
@@ -1,12 +1,160 @@
 package holonomicEV3_server;
 
-public class HolonomicEV3Server
+import java.io.*;
+import java.net.*;
+import lejos.hardware.*;
+import lejos.hardware.motor.*;
+import lejos.hardware.port.*;
+import lejos.hardware.sensor.MindsensorsAbsoluteIMU;
+import lejos.robotics.navigation.OmniPilot;
+import lejos.robotics.GyroscopeAdapter;
+import lejos.robotics.SampleProvider;
+import lejos.hardware.ev3.LocalEV3;
+
+@SuppressWarnings("deprecation")
+public class HolonomicEV3Server extends Thread
 {
+	public static final int CLOSE = 0;
+	public static final int JOYSTICK = 1;
+	public static final int KEYBOARD = 2;
+	
+	public static final int FORWARD = 10;
+	public static final int LEFT = 11;
+	public static final int RIGHT = 12;
+	public static final int BACKWARD = 13;
+	public static final int ROT_LEFT = 14;
+	public static final int ROT_RIGHT = 15;	
+	
+	public static final int PORT = 7360;
+	
+	private Socket client;
+	private static boolean running = true;
+	private static ServerSocket server;
 
-	public static void main(String[] args)
+	private OmniPilot pilot;
+	static Power battery = LocalEV3.get().getPower();
+	InputStream in;
+	
+	//private boolean absolute = false;
+	
+	public static void main(String[] args) throws IOException
 	{
-		// TODO Auto-generated method stub
+		server = new ServerSocket(PORT);
+		while (running)
+		{
+			System.out.println("Awaiting client..");
+			new HolonomicEV3Server(server.accept()).start();
+		}
+	}
+	
+	public HolonomicEV3Server(Socket client)
+	{
+		this.client = client;
+		Button.ESCAPE.addKeyListener(new EscapeListener());
+		pilot = new OmniPilot(16.0f, 5.0f, Motor.B, Motor.A, Motor.C, true, true, battery);
+		pilot.setLinearSpeed(80.0);
+	}
+	
+	public void run()
+	{
+		System.out.println("CLIENT CONNECT");
+		try
+		{
+			in = client.getInputStream();
+			DataInputStream dIn = new DataInputStream(in);
+			while (client != null)
+			{
+				String receivedNumString = dIn.readUTF();
+				String[] numstrings = receivedNumString.split("@@");
+				int[] myMessageArray = new int[numstrings.length];
+				int indx = 0;
 
+				for (String numStr : numstrings)
+				{
+					myMessageArray[indx++] = Integer.parseInt(numStr);
+				}
+
+				if (myMessageArray[0] == CLOSE)
+				{
+					// close command
+					client.close();
+					client = null;
+				}
+				else if (myMessageArray[0] == 2)
+				{
+					// keyboard command
+					carAction(myMessageArray[1]);
+					// TODO: create absolute/relative switch
+				}
+				else if (myMessageArray[0] == 1)
+				{
+					// spaceNavigator
+					carAction(myMessageArray[1], myMessageArray[2], myMessageArray[3]);
+				}
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 
+	public void carAction(int command)
+	{
+		switch (command)
+		{
+			case FORWARD:
+				pilot.travel(20.0, 0.0);
+				break;
+			case BACKWARD:
+				pilot.travel(20, 180.0);
+				break;
+			case RIGHT:
+				pilot.travel(20, 270.0);
+				break;
+			case LEFT:
+				pilot.travel(20, 90.0);
+				break;
+			case ROT_LEFT:
+				pilot.rotate(-45);
+				break;
+			case ROT_RIGHT:
+				pilot.rotate(45);
+				break;
+		}
+	}
+	
+	// driving car with spaceNavigator
+	@SuppressWarnings("deprecation")
+	public void carAction(int linearSpeed, int angle, int angularSpeed)
+	{
+
+		//pilot.moveStraight(linearSpeed, (int) angle);
+		
+		// The spinning move method is more sophisticated but difficult to control
+		pilot.spinningMove(linearSpeed, angularSpeed, (int) angle);
+			
+		// Uncomment for debugging data
+		// System.out.println("S: "+linearSpeed+" A: "+angle+" Com: "+ev3Sample[0]+" C:
+		// "+(int) (-ev3Sample[0]+angle)+" Sp: "+angularSpeed);
+	}
+	
+	public class EscapeListener implements KeyListener
+	{
+
+		public void keyPressed(Key k)
+		{
+			running = false;
+			System.out.println("Exiting...");
+			System.exit(0);
+		}
+
+		public void keyReleased(Key k)
+		{
+		}
+	}
 }
